5b8255ef2750f39cb1e02b493388c40893ad789b
YASSER-TAIBI <yasser.taibi.19@gmail.com>
2025-12-31 08:19:59 +0100

    Initialisation du projet Spring Boot foot5connect

---
+++ foot5connect/src/main/java/com/yazzer/foot5connect/Foot5connectApplication.java
@@ -0,0 +1,13 @@
+package com.yazzer.foot5connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Foot5connectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Foot5connectApplication.class, args);
+	}
+
+}
---
+++ foot5connect/src/test/java/com/yazzer/foot5connect/Foot5connectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yazzer.foot5connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Foot5connectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
