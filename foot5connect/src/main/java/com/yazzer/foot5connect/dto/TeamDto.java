a0b1bfc6aec40d67f086c0e00f3037b750c06bd1
YASSER-TAIBI <yasser.taibi.19@gmail.com>
2026-01-01 20:50:13 +0100

    implementation des DTOs (TeamDto,TeamMemberDto,UserDto)

---
+++ foot5connect/src/main/java/com/yazzer/foot5connect/dto/TeamDto.java
@@ -0,0 +1,69 @@
+package com.yazzer.foot5connect.dto;
+
+import com.yazzer.foot5connect.models.Team;
+import com.yazzer.foot5connect.models.TeamStatus;
+import com.yazzer.foot5connect.models.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class TeamDto {
+
+    private Integer id;
+
+    private String name;
+
+    private String logoUrl;
+
+    private String country;
+
+    private String city;
+
+    private TeamStatus status;
+
+    private Integer captainId;
+
+    public static TeamDto fromEntity(Team team) {
+        if (team == null){
+            return null;
+            // TODO throw an exception
+    }
+        return TeamDto.builder()
+                .id(team.getId())
+                .name(team.getName())
+                .logoUrl(team.getLogoUrl())
+                .country(team.getCountry())
+                .city(team.getCity())
+                .status(team.getStatus())
+                .captainId(team.getCaptain().getId())
+                .build();
+    }
+
+    public static Team toEntity(TeamDto teamDto) {
+        if (teamDto == null){
+            return null;
+            // TODO throw an exception
+        }
+        return Team.builder()
+                .id(teamDto.getId())
+                .name(teamDto.getName())
+                .logoUrl(teamDto.getLogoUrl())
+                .country(teamDto.getCountry())
+                .city(teamDto.getCity())
+                .status(teamDto.getStatus())
+                .captain(
+                        User.builder()
+                                .id(teamDto.getCaptainId())
+                                .build()
+                        )
+                .build();
+    }
+
+}
--- foot5connect/src/main/java/com/yazzer/foot5connect/models/AvailabilityStatus.java
+++ foot5connect/src/main/java/com/yazzer/foot5connect/models/AvailabilityStatus.java
@@ -1,6 +1,7 @@
 package com.yazzer.foot5connect.models;
 
 public enum AvailabilityStatus {
+    INDISPONIBLE,
     DISPONIBLE,
     EN_EQUIPE
 }
