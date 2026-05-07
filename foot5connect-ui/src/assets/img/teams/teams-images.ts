4384bbc290e916e532e4a0794f8d6dd02ff3302c
YASSER-TAIBI <yasser.taibi.19@gmail.com>
2026-05-08 00:09:11 +0200

    ajoute logo d'equipe

---
+++ foot5connect-ui/src/assets/img/teams/teams-images.ts
@@ -0,0 +1,14 @@
+export const TEAMS_IMAGES = {
+
+  team_01: 'https://res.cloudinary.com/ddsabo1fb/image/upload/v1778180933/team_01_xszrmy.webp',
+  team_02: 'https://res.cloudinary.com/ddsabo1fb/image/upload/v1778180934/team_02_lpx43j.webp',
+  team_03: 'https://res.cloudinary.com/ddsabo1fb/image/upload/v1778180934/team_03_uaawsw.webp',
+  team_04: 'https://res.cloudinary.com/ddsabo1fb/image/upload/v1778180936/team_04_szjred.webp',
+  team_05: 'https://res.cloudinary.com/ddsabo1fb/image/upload/v1778180939/team_05_she79x.webp',
+  team_06: 'https://res.cloudinary.com/ddsabo1fb/image/upload/v1778180939/team_06_uukh9n.webp',
+  team_07: 'https://res.cloudinary.com/ddsabo1fb/image/upload/v1778180941/team_07_os4hio.webp',
+  team_08: 'https://res.cloudinary.com/ddsabo1fb/image/upload/v1778180944/team_08_uoyske.webp',
+  team_09: 'https://res.cloudinary.com/ddsabo1fb/image/upload/v1778180944/team_09_i7xisp.webp',
+  team_10: 'https://res.cloudinary.com/ddsabo1fb/image/upload/v1778180946/team_10_l1em77.webp',
+
+} as const;
